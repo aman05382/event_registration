550d7c42333b6713467462c52a80e0802a3207b5
aman05382 <aman05382@gmail.com>
2020-05-15 14:38:06 +0530

    fix-form

--- register.php
+++ register.php
@@ -9,7 +9,7 @@ if (!$conn) {
 
 
 //Connect to database.
-$db = mysqli_select_db($conn, 'event_registration');
+$db = mysqli_select_db($conn, 'event');
 if (!$db) {
     die("Database Connection Failed" . mysqli_error($db));
 }
@@ -25,11 +25,11 @@ if (isset($_POST['submit'])) {
     $no_of_tickets = $_POST['no_of_tickets'];
 
 
-    $event = "INSERT INTO `participants`(`firstName`, `lastName`, `email`, `phone`, `idCard`, `registrationType`, `numberOfTickets`) VALUES ('$fname', '$lname', '$email', '$phone', '$id_card', '$registration_type', '$no_of_tickets')";
+    $event = "INSERT INTO `participants`(`FIRST-NAME`, `LAST-NAME`, `EMAIL`, `PHONE`, `ID-CARD`, `REGISTRATION-TYPE`, `NO_OF_TICKETS`) VALUES ('$fname', '$lname', '$email', '$phone', '$id_card', '$registration_type', '$no_of_tickets')";
         $register = mysqli_query($conn, $event);
         if ($register) {
             echo "<script>alert('Successfully entered details');</script>";
-            // header('location:Success.html');
+            header('location:index.html');
         } else {
             echo mysqli_error($conn);
         }
