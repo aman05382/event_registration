083e0775852f0cfaac6c9eb8d159fac09841c22e
RoshanB392 <roshanbonde30@gmail.com>
2020-06-06 00:30:20 +0530

    fixed some typos

--- admin/session.php
+++ admin/session.php
@@ -3,14 +3,14 @@
 // Establishing Connection with Server by passing server_name, user_id and password as a parameter
 $connection = mysqli_connect("localhost", "root", "");
 // Selecting Database
-$db = mysqli_select_db("event", $connection);
+$db = mysqli_select_db($connection, 'event');
 session_start();// Starting Session
 // Storing Session
 $user_check=$_SESSION['login_user'];
 // SQL Query To Fetch Complete Information Of User
-$ses_sql=mysqli_query("select USERNAME from login where USERNAME='$user_check'", $connection);
+$ses_sql=mysqli_query($connection, "SELECT `USERNAME` FROM `login` WHERE USERNAME='$user_check'");
 $row = mysqli_fetch_assoc($ses_sql);
-$login_session =$row['username'];
+$login_session =$row['USERNAME'];
 if(!isset($login_session)){
 mysqli_close($connection); // Closing Connection
 header('Location: index.php'); // Redirecting To Home Page
