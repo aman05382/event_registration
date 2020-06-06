3bab56fa41ca13cc1571b2453dba47f1169ee164
aman05382 <aman05382@gmail.com>
2020-06-06 17:13:11 +0530

    working

---
+++ admin/Analyze.php
@@ -0,0 +1,18 @@
+<?php
+
+// require('../db.php');
+// $currentday=date('Y/m/d');
+// echo $currentday;
+
+// $sql1="select NO_OF_TICKETS from participants where DATE='$currentday'";
+// $dataSet1=mysqli_query($conn,$sql1);
+// $NofStud=mysqli_fetch_array($dataSet1);
+
+// $sql2="select FOOD_REMAINING from participants where DATE='$currentday'";
+// $dataSet2=mysqli_query($conn,$sql2);
+// $foodrem=mysqli_fetch_array($dataSet2);
+
+// $sql3="select REGISTRATION-TYPE from participants where DATE='$currentday'";
+// $dataSet3=mysqli_query($conn,$sql3);
+// $TYPE=mysqli_fetch_array($dataSet3);
+?> 
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -51,7 +51,7 @@ include('session.php');
                 <div class="sb-sidenav-menu">
                     <div class="nav">
                         <div class="sb-sidenav-menu-heading">Home</div>
-                        <a class="nav-link" href="index.html">
+                        <a class="nav-link" href="dashboard.php">
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
